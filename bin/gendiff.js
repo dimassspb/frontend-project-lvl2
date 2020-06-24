a3e1df5cce8f944d35c07cbb52450ef6d28f9538
Dmitrii <eurosd@yandex.ru>
2020-06-24 11:02:40 +0300

    corrected

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
+const { program } = require('commander');
+//import program from 'commander';
+import genDiff from '..';
 
-import program from 'commander';
 program.version('0.0.1');
 
 program
-.description('Compares two configuration files and shows a difference.')
-.option('-f, --format [type]', 'output format')
-.arguments('<filepath1> <filepath2>');
-
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.action((filepath1, filepath2) => {
   const diff = genDiff(filepath1, filepath2, program.format);
   console.log(diff);
 });
-
 
 program.parse(process.argv);
