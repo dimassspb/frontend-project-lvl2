83efb79f29495b89aa1f4a16c578b334e53cec73
Dmitrii <eurosd@yandex.ru>
2020-06-25 11:59:31 +0300

    corrected

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
-import program from 'commander';
-import genDiff from '..';
 
-program.version('0.0.1');
+import startProgram from '../startProgram.js';
 
-program
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>');
-
-program.action((filepath1, filepath2) => {
-  const diff = genDiff(filepath1, filepath2, program.format);
-  console.log(diff);
-});
-
-program.parse(process.argv);
+startProgram();
---
+++ startProgram.js
@@ -0,0 +1,15 @@
+import program from 'commander';
+
+export default () => {
+  program.version('0.0.1');
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
+  program.action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2, program.format);
+    console.log(diff);
+  });
+
+  program.parse(process.argv);
+};
