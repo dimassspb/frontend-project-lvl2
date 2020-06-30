7bb57bba600a35256d510c568c775a3fd4a4d2d6
Dmitrii <eurosd@yandex.ru>
2020-06-30 11:53:54 +0300

    corrected

--- startProgram.js
+++ startProgram.js
@@ -1,14 +1,15 @@
 import program from 'commander';
+import makeDifference from './index.js';
 
 export default () => {
   program.version('0.0.1');
   program
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>');
-  program.action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2, program.format);
-    console.log(diff);
+    .arguments('<path1> <path2>');
+  program.action((path1, path2) => {
+    const result = makeDifference(path1, path2, program.format);
+    console.log(result);
   });
 
   program.parse(process.argv);
