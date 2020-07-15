722a1316f42935c40663719073ee2141967a2c92
Dmitrii <eurosd@yandex.ru>
2020-07-15 19:34:33 +0300

    implemented the stylish formatter

--- index.js
+++ index.js
@@ -4,38 +4,44 @@ import genDiff from './difference.js';
 import parse from './parsers.js';
 
 const readFile = (filePath) => {
-  const fileData = fs.readFileSync(path.resolve(filePath), 'utf-8');// unicode transformation format;
+  const fileData = fs.readFileSync(path.resolve(filePath), 'utf-8');
   const extnameFile = path.extname(filePath).slice(1);
   return parse(fileData, extnameFile);
 };
-
 const whiteSpace = '  ';
-const tree = (items) => {
-  const makeString = (data) => data.map(({
-    type, name, newValue, oldValue,
-  }) => {
-    switch (type) {
-      case 'compare':
-        return `${whiteSpace}  ${name}: {\n${makeString(newValue)}`;
-      case 'unchanged':
-        return `${whiteSpace}  ${name}: ${oldValue}`;
-      case 'removed':
-        return `${whiteSpace}- ${name}: ${oldValue}`;
-      case 'added':
-        return `${whiteSpace}+ ${name}: ${newValue}`;
-      case 'changed':
-        return `${whiteSpace}+ ${name}: ${newValue}\n${whiteSpace}- ${name}: ${oldValue}`;
-      default:
-        throw new Error(`Unknown type: ${type}`);
-    }
-  }).join('\n');
-  return `{\n${makeString(items)}\n}`;
+const stringify = (data, indent) => {
+  if (!(data instanceof Object)) {
+    return data;
+  }
+  return Object.entries(data).map(([key, value]) => `{\n${indent}${whiteSpace.repeat(3)}${key}: ${value}\n${indent}${whiteSpace}}`);
 };
-
+const stylish = (items) => {
+  const makeString = (data, indentCounter) => {
+    const indent = whiteSpace.repeat(indentCounter);
+    return data.map(({
+      type, name, newValue, oldValue,
+    }) => {
+      switch (type) {
+        case 'added':
+          return `${indent}+ ${name}: ${stringify(newValue, indent)}`;
+        case 'removed':
+          return `${indent}- ${name}: ${stringify(oldValue, indent)}`;
+        case 'changed':
+          return `${indent}+ ${name}: ${stringify(newValue, indent)}\n${indent}- ${name}: ${stringify(oldValue, indent)}`;
+        case 'unchanged':
+          return `${indent}  ${name}: ${stringify(oldValue, indent)}`;
+        case 'compare':
+          return `${indent}  ${name}: {\n${makeString(newValue, indentCounter + 2)}\n${indent}${whiteSpace}}`;
+        default:
+          throw new Error(`Unknown state: ${type}`);
+      }
+    }).join('\n');
+  };
+  return `{\n${makeString(items, 1)}\n}`;
+};
 const makeDifference = (path1, path2, format) => {
   const dataBefore = readFile(path1);
   const dataAfter = readFile(path2);
-  return tree(genDiff(dataBefore, dataAfter), format);
+  return stylish(genDiff(dataBefore, dataAfter), format);
 };
-
 export default makeDifference;
