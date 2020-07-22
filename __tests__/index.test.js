c8f4c666d45e5bb58e8a320dbff2ece3f7a56a45
Dmitrii <eurosd@yandex.ru>
2020-07-22 16:54:28 +0300

    added tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,72 +1,65 @@
 import path from 'path';
-import makeDifference from '../index.js';
+import fs from 'fs';
+import makeDifference from '../src/index.js';
 
 const commonPath = '__tests__/__fixtures__/';
-const expected = `{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
+
+const makePath = (fileName) => path.join(commonPath, fileName);
+
+const readFixture = (fixtureName) => {
+  const fixturePath = makePath(fixtureName);
+  const fixtureContent = fs.readFileSync(fixturePath, 'utf-8').trim();
+  return fixtureContent;
+};
 
 test('Сomparing two flat .json files', () => {
     const path1 = path.join(commonPath, 'before.json');
     const path2 = path.join(commonPath, 'after.json');
-expect(makeDifference(path1, path2)).toBe(expected);
+    const expected = readFixture('flatStylishResult.txt');
+    const format = 'stylish';
+expect(makeDifference(path1, path2, format)).toBe(expected);
 });
 
 test('Сomparing two flat .yaml files', () => {
     const path1 = path.join(commonPath, 'before.yaml');
     const path2 = path.join(commonPath, 'after.yaml');
-expect(makeDifference(path1, path2)).toBe(expected);
+    const expected = readFixture('flatStylishResult.txt');
+    const format = 'stylish';
+expect(makeDifference(path1, path2, format)).toBe(expected);
 });
 
 test('Сomparing two flat .ini files', () => {
-    const path1 = path.join(commonPath, 'filepath1.json');
-    const path2 = path.join(commonPath, 'filepath2.json');
-expect(makeDifference(path1, path2)).toBe(expected1);
+    const path1 = path.join(commonPath, 'before.ini');
+    const path2 = path.join(commonPath, 'after.ini');
+    const expected = readFixture('flatStylishResult.txt');
+    const format = 'stylish';
+expect(makeDifference(path1, path2, format)).toBe(expected);
 });
 
 test('Сomparing two .json files', () => {
-    const path1 = path.join(commonPath, 'before.ini');
-    const path2 = path.join(commonPath, 'after.ini');
-expect(makeDifference(path1, path2)).toBe(expected);
+    const path1 = path.join(commonPath, 'filepath1.json');
+    const path2 = path.join(commonPath, 'filepath2.json');
+    const expected = readFixture('recursionStylishResult.txt');
+    const format = 'stylish';
+expect(makeDifference(path1, path2, format)).toBe(expected);
 });
 
-const expected1 = `{
-    common: {
-        setting1: Value 1
-      - setting2: 200
-      + setting3: {
-            key: value
-        }
-      - setting3: true
-        setting6: {
-            key: value
-          + ops: vops
-        }
-      + follow: false
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-    }
-    group1: {
-      + baz: bars
-      - baz: bas
-        foo: bar
-      + nest: str
-      - nest: {
-            key: value
-        }
-    }
-  - group2: {
-        abc: 12345
-    }
-  + group3: {
-        fee: 100500
-    }
-}`;
+test('Сomparing two .json files format = plain', () => {
+    const path1 = path.join(commonPath, 'filepath1.json');
+    const path2 = path.join(commonPath, 'filepath2.json');
+    const expected = readFixture('plainResult.txt');
+const format = 'plain';
+expect(makeDifference(path1, path2, format)).toBe(expected);
+});
+
+
+test('Сomparing two .json files format = json', () => {
+    const path1 = path.join(commonPath, 'filepath1.json');
+    const path2 = path.join(commonPath, 'filepath2.json');
+    const expected = readFixture('jsonResult.txt');
+    const format = 'json';
+expect(makeDifference(path1, path2, format)).toBe(expected);
+});
+
+
 
