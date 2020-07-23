1e8559824ef7421767f689c13f3871dd2fc978b3
Dmitrii <eurosd@yandex.ru>
2020-07-23 11:33:29 +0300

    corrected

--- src/startProgram.js
+++ src/startProgram.js
@@ -5,7 +5,7 @@ export default () => {
   program.version('0.0.1');
   program
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'output format', 'tree')
+    .option('-f, --format <type>', 'output format', 'stylish')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig) => {
       const result = makeDifference(firstConfig, secondConfig, program.format);
