fda6e8616fe0a58564c858af4a51f34bb15d906e
Dmitrii <eurosd@yandex.ru>
2020-08-19 22:58:26 +0300

    fixed errors

--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@ const getData = (filePath) => {
 export default (path1, path2, format = 'stylish') => {
   const dataBefore = getData(path1);
   const dataAfter = getData(path2);
-  return render(buildAst(dataBefore, dataAfter), format);
+  const buildDiff = buildAst(dataBefore, dataAfter);
+  return render(buildDiff, format);
 };
